5ee78b424216f5d0d7e2db59f9f82a940f1ced41
Mateusz Staniuk <staniuk.mateusz@gmail.com>
2020-03-20 22:10:40 +0100

    Add better map presentation, Resent reuest to api after map move

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,41 +1,104 @@
 <template>
   <div class="map">
-    <LMap :zoom="zoom" :center="center">
+    <LMap :zoom="zoom" :center="currentPosition" ref="map">
       <l-tile-layer :url="url" :attribution="attribution" />
-      <l-marker
+      <l-circle-marker
         v-for="point in poi"
         :key="point.latLng.toString()"
+        :stroke="false"
         :lat-lng="point.latLng"
-      />
+        :radius="point.density * 2"
+        :fillColor="getCircleColorByDensity(point.density)"
+        :fillOpacity="1"
+      >
+        <l-popup>
+          <article>
+            <header>
+              <h2>
+                {{point.name}}
+              </h2>
+              <div>
+                Zagrożenie: {{ point.density > 10 ? 'Duże' : 'Małe' }}
+              </div>
+              <div>
+                Zagęszczenie: {{point.density}}
+              </div>
+              <div>
+                <small>pos: {{point.lat}}, {{point.lng}}</small>
+              </div>
+            </header>
+          </article>
+        </l-popup>
+      </l-circle-marker>
     </LMap>
   </div>
 </template>
 
 <script>
 import L from "leaflet";
-import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { LMap, LTileLayer, LPopup, LCircleMarker } from "vue2-leaflet";
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    LPopup,
+    LCircleMarker,
   },
   props: {
     poi: {
       type: Array,
       default: () => []
+    },
+    position: {
+      type: Object,
+      default: () => {}
     }
   },
   data() {
     return {
-      zoom: 16,
-      center: L.latLng(50.049538, 19.942908),
+      zoom: 18,
       url:
         "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     };
+  },
+  methods: {
+    getCircleColorByDensity(density) {
+      if (density < 5) {
+        return 'rgba(255, 255, 204, 0.33)';
+      }
+      if (density < 10) {
+        return 'rgba(255, 255, 204, 0.5)';
+      }
+      if (density < 15) {
+        return 'rgba(255, 204, 153, 0.5)';
+      }
+      if (density < 20) {
+        return 'rgba(255, 153, 102, 0.5)';
+      }
+      if (density < 30) {
+        return 'rgba(255, 102, 0, 0.5)';
+      }
+      if (density < 50) {
+        return 'rgba(255, 0, 0, 0.5)';
+      }
+
+      return 'rgba(255, 0, 0, 0.75)';
+    }
+  },
+  computed: {
+    currentPosition() {
+      return L.latLng(this.position.lat, this.position.lng);
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$refs.map.mapObject.on('moveend', (ev) => {
+        this.$emit('moved', ev);
+      });
+    });
   }
 };
 </script>
--- src/views/CrowdedPlaces.vue
+++ src/views/CrowdedPlaces.vue
@@ -1,5 +1,30 @@
 <template>
-  <Map :poi="poi" />
+  <section class="places">
+    <header class="places__header">
+      <h1 class="places__title brygada">
+        Crowded places
+      </h1>
+
+      <div class="places__description">
+        Here is the map with some density indicators which should help you <br />
+        to avoid crowdy places. It's really important - <a>read why</a>
+      </div>
+    </header>
+    <div class="places__actions">
+      <div class="places__action">
+        <!--          <Search />-->
+        search
+      </div>
+      <div class="places__action">
+        <!--          <Filters />-->
+        filters
+      </div>
+    </div>
+
+    <div class="places__content">
+      <Map :poi="poi" :position="position" @moved="onMovedHandler"/>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -11,26 +36,84 @@ export default {
   components: { Map },
   data() {
     return {
-      user: {
+      position: {
         lat: 50.049679,
         lng: 19.947022
       },
       poi: []
     };
   },
-  async mounted() {
-    try {
-      const response = await this.$api.getPoiByMapCenter(this.user);
-      return response.data.map(r => {
-        const latLng = L.latLng(r.loc[0].lat, r.loc[0].lng);
-        return {
-          ...r,
-          latLng
-        };
-      });
-    } catch (e) {
-      console.log(e);
+  methods: {
+    onMovedHandler(ev) {
+      this.position = ev.target.getCenter();
+    },
+    async getPoiByPosition() {
+      try {
+        const response = await this.$api.getPoiByMapCenter(this.position);
+        const newPoi = response.data.map(r => {
+          const latLng = L.latLng(r.lat, r.lng);
+          return {
+            ...r,
+            latLng
+          };
+        });
+
+        this.poi = [
+          ...this.poi.filter(p => newPoi.findIndex(np => np.name === p.name) < 0),
+          ...newPoi
+        ]
+      } catch (e) {
+        console.log(e);
+      }
     }
+  },
+  watch: {
+    position() {
+      this.getPoiByPosition();
+    }
+  },
+  async mounted() {
+    this.getPoiByPosition();
   }
 };
 </script>
+
+<style lang="scss">
+  .places {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    &__header {
+      background-color: #ffffff;
+      padding: 40px;
+      color: #302b5c;
+      flex: 0 0 auto;
+    }
+
+    &__title {
+      font-size: 40px;
+    }
+
+    &__description {
+      a {
+        font-weight: bold;
+        text-decoration: underline;
+      }
+    }
+    &__actions {
+      flex: 0 0 auto;
+      padding: 12px;
+      display: flex;
+      background-color: #e8e8e8;
+    }
+    &__action {
+      & + & {
+        margin-left: 24px;
+      }
+    }
+    &__content {
+      flex: 1 1 100%;
+    }
+  }
+</style>
