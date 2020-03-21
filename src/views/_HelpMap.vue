685a8b36b2b1530e6f301f3a111094e0ea2e0093
Wojciech Bąk <wojtek@butio.pl>
2020-03-21 13:25:22 +0100

    Form control: switch, adjustment bar in HelpMap view

--- src/views/_HelpMap.vue
+++ src/views/_HelpMap.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="help-map">
-    HELP MAP
+    <div class="help-map__bar adjustment">
+      <SwitchControl v-model="showTasks" :color="'blue'">
+        Single tasks
+      </SwitchControl>
+
+      <SwitchControl v-model="showActions" :color="'red'">
+        Crowd actions
+      </SwitchControl>
+    </div>
   </div>
 </template>
 
 <script>
+import SwitchControl from "@/components/SwitchControl";
+
 export default {
-  name: "HelpMap"
+  name: "HelpMap",
+
+  components: { SwitchControl },
+
+  data() {
+    return {
+      showTasks: true,
+      showActions: false
+    };
+  }
 };
 </script>
