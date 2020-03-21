de13eed49c909e6f0f14cf2186023f77de4455f3
Mateusz Staniuk <staniuk.mateusz@gmail.com>
2020-03-21 19:50:28 +0100

    Add filter button

---
+++ src/components/FiltersButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="filters-button">
+    <svg class="filters-button__icon" viewBox="0 0 64 64" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <g stroke="none" stroke-width="1" fill-rule="evenodd">
+        <path d="M16,14 L16,22.1539297 L27.3898489,34.8193906 L27.3898489,44.4791328 L36.6102189,50 L36.6102189,34.8193906 L48,22.1539297 L48,14 L16,14 Z M18.7118656,16.8145391 L45.2882699,16.8145391 L45.2882699,19.6291484 L45.2882699,19.6291484 L18.7118656,19.6291484 L18.7118656,16.8145391 Z M30.1017144,42.8536484 L30.1017144,33.7100703 L19.9700725,22.4436172 L44.0301307,22.4436172 L33.8982856,33.7100703 L33.8982856,45.1269922 L30.1017144,42.8536484 Z" id="Fill-1"></path>
+      </g>
+    </svg>
+    Set filters
+  </button>
+</template>
+
+<style lang="scss">
+  .filters-button {
+    border: 2px solid #3d3d72;
+    padding: 4px 16px;
+    background-color: transparent;
+    color: #3d3d72;
+    fill: #3d3d72;
+    border-radius: 20px;
+    font-size: 14px;
+    font-weight: bold;
+    cursor: pointer;
+
+    &:hover,
+    &:focus,
+    &:active {
+      background-color: #3d3d72;
+      color: #ffffff;
+      fill: #ffffff;
+    }
+
+    &__icon {
+      width: 20px;
+      height: 20px;
+      fill: inherit;
+      vertical-align: middle;
+    }
+  }
+</style>
--- src/views/CrowdedPlaces.vue
+++ src/views/CrowdedPlaces.vue
@@ -6,116 +6,157 @@
       </template>
       <template v-slot:text>
         Here is the map with some density indicators which should help you
-        <br />
+        <br/>
         to avoid crowdy places. It's really important -
         <a href="#">read why</a>
       </template>
     </PageHead>
     <div class="places__actions">
       <div class="places__action">
-        <!--          <Search />-->
-        search
+        <Search/>
       </div>
       <div class="places__action">
-        <!--          <Filters />-->
-        filters
+        <FiltersButton @click.native="searchClickHandler"/>
+      </div>
+
+      <div
+        class="places__actions-bar"
+        :class="{
+          'places__actions-bar--visible': actionsBarVisible
+        }"
+      >
+
       </div>
     </div>
 
     <div class="places__content">
-      <Map :poi="poi" :position="position" @moved="onMovedHandler" />
+      <Map :poi="poi" :position="position" @moved="onMovedHandler"/>
     </div>
   </section>
 </template>
 
 <script>
-import Map from "@/components/Map.vue";
-import PageHead from "@/components/PageHead.vue";
-import L from "leaflet";
+  import Map from '@/components/Map.vue';
+  import PageHead from '@/components/PageHead.vue';
+  import FiltersButton from '@/components/FiltersButton.vue';
+  import L from 'leaflet';
 
-export default {
-  name: "Home",
-  components: { Map, PageHead },
-  data() {
-    return {
-      position: {
-        lat: 50.049679,
-        lng: 19.947022
-      },
-      poi: []
-    };
-  },
-  watch: {
-    position() {
-      this.getPoiByPosition();
-    }
-  },
-  async mounted() {
-    this.getPoiByPosition();
-  },
-  methods: {
-    onMovedHandler(ev) {
-      this.position = ev.target.getCenter();
+  export default {
+    name: 'Home',
+    components: {Map, PageHead, FiltersButton},
+    data() {
+      return {
+        actionsBarVisible: false,
+        position: {
+          lat: 50.049679,
+          lng: 19.947022
+        },
+        poi: []
+      };
     },
-    async getPoiByPosition() {
-      try {
-        const response = await this.$api.getPoiByMapCenter(this.position);
-        const newPoi = response.data.map(r => {
-          const latLng = L.latLng(r.lat, r.lng);
-          return {
-            ...r,
-            latLng
-          };
-        });
+    watch: {
+      position() {
+        this.getPoiByPosition();
+      }
+    },
+    async mounted() {
+      this.getPoiByPosition();
+    },
+    methods: {
+      searchClickHandler() {
+        this.actionsBarVisible = !this.actionsBarVisible;
+      },
+      onMovedHandler(ev) {
+        this.position = ev.target.getCenter();
+      },
+      async getPoiByPosition() {
+        try {
+          const response = await this.$api.getPoiByMapCenter(this.position);
+          const newPoi = response.data.map(r => {
+            const latLng = L.latLng(r.lat, r.lng);
+            return {
+              ...r,
+              latLng
+            };
+          });
 
-        this.poi = [
-          ...this.poi,
-          ...newPoi.filter(np => this.poi.findIndex(p => np.name === p.name) < 0)
-        ]
-      } catch (e) {
-        console.log(e);
+          this.poi = [
+            ...this.poi,
+            ...newPoi.filter(np => this.poi.findIndex(p => np.name === p.name) < 0)
+          ]
+        } catch (e) {
+          console.log(e);
+        }
       }
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss">
-.places {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-
-  &__header {
-    background-color: #ffffff;
-    padding: 40px;
-    color: #302b5c;
-    flex: 0 0 auto;
-  }
-
-  &__title {
-    font-size: 40px;
-  }
-
-  &__description {
-    a {
-      font-weight: bold;
-      text-decoration: underline;
-    }
-  }
-  &__actions {
-    flex: 0 0 auto;
-    padding: 12px;
+  .places {
     display: flex;
-    background-color: #e8e8e8;
-  }
-  &__action {
-    & + & {
-      margin-left: 24px;
+    flex-direction: column;
+    height: 100%;
+
+    &__header {
+      background-color: #ffffff;
+      padding: 40px;
+      color: #302b5c;
+      flex: 0 0 auto;
+    }
+
+    &__title {
+      font-size: 40px;
+    }
+
+    &__description {
+      a {
+        font-weight: bold;
+        text-decoration: underline;
+      }
+    }
+
+    &__actions {
+      position: relative;
+      z-index: 2;
+      flex: 0 0 auto;
+      padding: 12px;
+      display: flex;
+      background-color: #e8e8e8;
+    }
+
+    &__action {
+      & + & {
+        margin-left: 24px;
+      }
+    }
+
+    &__actions-bar {
+      transition: transform 250ms ease-in-out,  visibility 0ms 250ms ease-in-out;
+      background-color: #e8e8e8;
+      position: absolute;
+      width: 100%;
+      height: 30px;
+      top: 100%;
+      left: 0;
+      right: 0;
+      transform: scaleY(0);
+      visibility: hidden;
+      pointer-events: none;
+      transform-origin: top;
+
+      &--visible {
+        transition: transform 250ms ease-in-out;
+        transform: scaleY(1);
+        visibility: visible;
+        pointer-events: all;
+      }
+    }
+
+    &__content {
+      position: relative;
+      flex: 1 1 100%;
+      z-index: 1;
     }
   }
-  &__content {
-    flex: 1 1 100%;
-  }
-}
 </style>
