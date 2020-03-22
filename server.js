c8265b3af0d4e9bdecd72a0a3bb6b5ceb7920806
Wojciech Bąk <wojtek@butio.pl>
2020-03-22 13:01:15 +0100

    Update server config to support Vue routes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+const history = require("connect-history-api-fallback");
 
 app = express();
+
+app.use(history());
 app.use(serveStatic(path.join(__dirname, "dist")));
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("Listening...");
