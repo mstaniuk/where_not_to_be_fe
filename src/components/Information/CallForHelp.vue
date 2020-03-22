4e1ee6a6cec00617eeb5fcb901cfacb2d477ab66
Wojciech Bąk <wojtek@butio.pl>
2020-03-22 14:25:27 +0100

    Change CallForHelp button

--- src/components/Information/CallForHelp.vue
+++ src/components/Information/CallForHelp.vue
@@ -1,7 +1,7 @@
 <template>
   <card class="call-help">
     <div class="call-help__question">Are you feeling bad?</div>
-    <button class="btn btn--small btn--filled btn--red">Call for help</button>
+    <button class="btn btn--small btn--contour btn--red">Call for help</button>
   </card>
 </template>
 
